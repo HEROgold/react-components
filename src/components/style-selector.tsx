5314b79deb7b94ba4ae3f8f9ac2ea1e1fe915969
HEROgold <martijnwieringa28@gmail.com>
2026-04-06 14:58:17 +0200

    Add function to determine initial color for StyleSelector input

--- src/components/style-selector.tsx
+++ src/components/style-selector.tsx
@@ -1,11 +1,19 @@
 import "@styles/theme.css";
 
+function findStartingColor(): string {
+  return (
+    getComputedStyle(document.documentElement).getPropertyValue('--base-color')
+    || getComputedStyle(document.documentElement).getPropertyValue("color").trim()
+    || "#000000"
+  );
+}
+
 export default function StyleSelector() {
-  // TODO: Initially set the color based on the document style
   return (
     <div className="style-selector">
       <input 
       type="color"
+      defaultValue={findStartingColor()}
       onChange={(e) => {
         document.documentElement.style.setProperty('--base-color', e.target.value);
       }}
